da32116fbfdb021bead355cad0e646de1b612050
Jason <bjason@gmail.com>
2014-10-05 23:58:33 -0700

    My dir.

--- gitit.py
+++ gitit.py
@@ -3,11 +3,14 @@ import os
 import sys
 
 
-my_dir = sys.argv[0]
-my_repo = "/home/jason/playground/"
+my_repo = sys.argv[0]
+my_dir = "/home/jason/playground/"
 
 if len(sys.argv) > 1:
     my_repo = sys.argv[1]
+
+
+my_dir = my_dir + my_repo
 
 directory = os.path.dirname(my_dir)
 
@@ -18,6 +21,7 @@ print "my_repo"
 print my_repo
 
 
-
 if no os.path.exists(directory):
     os.mkdirs(directory)
+
+
