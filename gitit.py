1f1aa6cb7f1a32255c7fc30b53c2af3a9879ac0a
Jason <bjason@gmail.com>
2014-10-06 00:08:54 -0700

    rint

--- gitit.py
+++ gitit.py
@@ -1,7 +1,5 @@
 import os
-
 import sys
-
 
 my_repo = sys.argv[0]
 my_dir = "/home/jason/playground/"
@@ -9,9 +7,7 @@ my_dir = "/home/jason/playground/"
 if len(sys.argv) > 1:
     my_repo = sys.argv[1]
 
-
 my_dir = my_dir + my_repo
-
 directory = os.path.dirname(my_dir)
 
 print "my_dir" 
@@ -20,6 +16,9 @@ print my_dir
 print "my_repo"
 print my_repo
 
+
+print "directory"
+print directory
 
 if no os.path.exists(directory):
     os.mkdirs(directory)
