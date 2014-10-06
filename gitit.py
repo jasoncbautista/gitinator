04c3948d993ab9d94edbf9aff8d936dd7770ee96
Jason <bjason@gmail.com>
2014-10-06 00:28:18 -0700

    better ditr

--- gitit.py
+++ gitit.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
     my_repo = sys.argv[1]
 
 directory = os.path.dirname(my_dir)
-directory = directory + my_repo
+directory = directory  + "/" + my_repo
 
 print "my_dir" 
 print my_dir
@@ -21,7 +21,7 @@ print "directory"
 print directory
 
 if not os.path.exists(directory):
-    os.mkdirs(directory)
+    os.mkdir(directory)
     os.chdir(directory)
 
 
