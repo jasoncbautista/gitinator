ea18988cf2aeffd462a0bdcd253a35547d1e91d9
Jason <bjason@gmail.com>
2014-10-05 23:10:32 -0700

    sys

--- gitit.py
+++ gitit.py
@@ -1,8 +1,12 @@
 import os
 
+import sys
 
 
-directory = os.path.dirname(dir)
+my_dir = sys.argv[0]
+my_repo = sys.argv[1]
+
+directory = os.path.dirname(my_dir)
 
 if no os.path.exists(directory):
     os.mkdirs(directory)
