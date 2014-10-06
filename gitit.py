40a4af5a72fdf7f691568556e9947435539148ed
Jason <bjason@gmail.com>
2014-10-05 23:56:43 -0700

    Repos.

--- gitit.py
+++ gitit.py
@@ -4,14 +4,20 @@ import sys
 
 
 my_dir = sys.argv[0]
-my_repo = sys.argv[1]
+my_repo = "/home/jason/playground/"
+
+if len(sys.argv) > 1:
+    my_repo = sys.argv[1]
 
 directory = os.path.dirname(my_dir)
 
+print "my_dir" 
+print my_dir
 
-import pdb
+print "my_repo"
+print my_repo
 
-pdb.set_trace()
+
 
 if no os.path.exists(directory):
     os.mkdirs(directory)
